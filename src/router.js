1d8b1df8b8cf997861e2c1ca025d9c43098e6f94
iLLviA <illvia.3594@gmail.com>
2020-08-01 18:45:29 +0900

    fix

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home'
 import Main from './views/Main'
 
 Vue.use(Router)
@@ -10,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/app',
       name: 'app',
       component: Main
     }
